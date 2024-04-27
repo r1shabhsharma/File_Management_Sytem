4bee1ecfd4fc6392716b8c32733d08cb0abd9995
Grishma03117 <2023mt03117@wilp.bits-pilani.ac.in>
2024-04-27 14:47:12 +0530

    support for duplicate file handling

--- n2/server.py
+++ n2/server.py
@@ -2,8 +2,20 @@ import hashlib
 import os
 import sys
 import threading
-import time
 from xmlrpc.server import SimpleXMLRPCServer
+
+class FileData:
+    def __init__(self, file_name, file_hash):
+        self.file_name = file_name
+        self.file_hash = file_hash
+
+    def __eq__(self, other):
+        return self.file_name == other.file_name and self.file_hash == other.file_hash
+    
+    # This hash is different from the file content hashing we use to check duplication
+    def __hash__(self):
+        # This is done because when we add custom objects to a set, Python needs a way to determine whether two objects are equal and to quickly locate objects in the data structure.
+        return hash((self.file_name, self.file_hash))
 
 hashCodes = set()
 
@@ -21,7 +33,6 @@ class FileSystemRPC:
         self.serverId = serverId
         self.fileDirectory = fileDirectory
         self.lock = threading.Lock()
-        self.hashFiles = []
 
     def generate_hash(self, content):
         # Generate a hash value based on the content using SHA-256
@@ -30,8 +41,8 @@ class FileSystemRPC:
         return hash_value
 
     # function for server to update the received file on its local storage
-    def updateFile(self, fileName, fileData):
-        print('Updating file:', fileName)
+    def updateFile(self, fileName, fileData, replaceFile=False):
+        print('Trying to Upload file:', fileName)
         with self.lock:
             currentPath = os.getcwd()
             serverPath = os.path.join(currentPath, 'local-files')
@@ -39,16 +50,25 @@ class FileSystemRPC:
             time.sleep(5)
 
             filePath = os.path.join(serverPath, fileName)
-            combined_data = fileName + fileData['content']
-            fileHashCode = hashlib.sha256(combined_data.encode()).hexdigest()
+            fileHashCode = self.generate_hash(fileName + fileData['content'])
+            
 
-            if filePath and fileHashCode in hashCodes:
-                return 'A file with the same name and content already exists' 
-            else:
-                with open(filePath, 'w') as file:
-                    file.write(fileData['content'])
-                    hashCodes.add(fileHashCode)
-                    return "File updated successfully"  # Return success message
+            for hashed_file_data in hashCodes:
+                if fileHashCode == hashed_file_data.file_hash and filePath:
+                    return 'A file with the same name and content already exists'
+            
+            if fileExists(filePath):
+                if not replaceFile:
+                    return 'User choice'
+                for hashed_file_data in hashCodes:
+                    print(hashed_file_data.file_name)
+                    if(fileName == hashed_file_data.file_name):
+                        hashed_file_data.file_hash = self.generate_hash(fileName + fileData['content'])
+                        
+            with open(filePath, 'w') as file:
+                file.write(fileData['content'])
+                hashCodes.add(FileData(fileName, fileHashCode))
+                return "File updated successfully"  # Return success message
                 
     # Function to fetch the file
     def getFile(self, fileName):
@@ -80,8 +100,7 @@ def setHashCodes(fileDirectory):
         fileData = getFileData(filePath)
         combined_data = fileName + fileData
         hashValue = hashlib.sha256(combined_data.encode()).hexdigest()
-        hashCodes.add(hashValue)
-        print(hashCodes)
+        hashCodes.add(FileData(fileName, hashValue))
 
 if __name__ == "__main__":
     address = 'localhost'
